00d2d3da01a4ca634b46bdae97ba8dae85149ff4
isaiahleonard <40407299+isaiahleonard@users.noreply.github.com>
2021-12-26 19:08:18 -0600

    Create AOC2021_D001_P001.pl

---
+++ AOC2021_D001_P001.pl
@@ -0,0 +1,30 @@
+use strict;
+use warnings;
+
+#
+# Advent Of Code - Day 001 Part 001
+#
+
+sub main 
+{
+  my $idx = 0;
+  my $ttl = 0;
+  my $nm1;
+  my $nm2;
+
+  foreach $nm2 (<STDIN>)
+  {
+    # We only perform checks with a pair of numbers.
+    if ($idx > 0 && $nm2 > $nm1)
+    {
+      $ttl += 1;
+    }
+    # Shift forward to get the next pair of numbers.
+    $nm1 = $nm2;
+    $idx += 1;
+  }
+
+  print "Total: $ttl";
+}
+
+main();
